9b7ce4373e336179598d882ba6871e59bd376bc0
Luana Giusto <luanagiusto@gmail.com>
2022-07-04 20:32:47 -0300

    componente de reviews

--- src/app/restaurants-details/reviews/reviews.component.ts
+++ src/app/restaurants-details/reviews/reviews.component.ts
@@ -1,4 +1,8 @@
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+
 import { Component, OnInit } from '@angular/core';
+import { RestaurantsService } from 'app/restaurants/restaurants.service';
 
 @Component({
   selector: 'mt-reviews',
@@ -7,9 +11,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReviewsComponent implements OnInit {
 
-  constructor() { }
+  reviews: Observable<any>;
+
+  constructor(
+    private restaurantsService: RestaurantsService,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.reviews = this.restaurantsService.reviewsOfRestaurant(this.route.parent.snapshot.params['id'])
   }
 
 }
